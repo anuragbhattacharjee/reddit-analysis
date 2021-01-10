e4641aa817a12981ffbc87481e461e1cbfa9a278
Anurag B <anurag@chatleads.io>
2021-01-10 22:37:28 +0600

    Data analysis of submissions and authors of two subreddits

--- app/__main__.py
+++ app/__main__.py
@@ -2,7 +2,7 @@ import json
 import logging
 import os
 from utils.scraper import scrape
-
+from utils.analysis import analyse
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
@@ -15,20 +15,31 @@ if __name__ == '__main__':
 
     logger.info(f'Starting app...')
 
-    # logger.info(
-    #     f'Please type the name of the subreddits you want to scrape and compare.')
-    # subreddit1 = input("First Subreddit: ")
-    # subreddit2 = input("Second subreddit: ")
+    print(f'Please type the name of the subreddits you want to scrape and compare.')
 
-    # logger.info(
-    #     f'Please type the from and to date in MM-YYYY format.')
-    # from_date = input("From: ")
-    # to_date = input("To: ")
+    subreddit1 = input("First Subreddit: ")
+    subreddit2 = input("Second subreddit: ")
 
-    # logger.info(
-    #     f'Scraping data from r/{subreddit1} and r/{subreddit2}')
+    print(f"For seeing analysis press 1, for scraping press 2")
 
-    # scrape(subreddit1, from_date, to_date)
-    # scrape("emacs", "01-2020", "03-2020")
-    scrape("emacs", "01-2020", "03-2020")
+    flag = int(input("Key: "))
+
+    if flag == 1:
+        analyse(subreddit1, subreddit2)
+    elif flag == 2:
+        print(
+            f'Please type the from and to date in MM-YYYY format.')
+        from_date = input("From: ")
+        to_date = input("To: ")
+
+        logger.info(
+            f'Scraping data from r/{subreddit1} and r/{subreddit2}')
+        
+        scrape(subreddit1, from_date, to_date)
+        scrape(subreddit2, from_date, to_date)
+    else:
+        logger.error("Invalid Key!")    
+
+
+
     
